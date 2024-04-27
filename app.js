b20962ff1b012f5cf8a8eaa08fa989fd8bae2e09
NemontovMaksim <149705847+NemontovMaksim@users.noreply.github.com>
2024-04-27 09:20:24 +0300

    new попытка
    )))))))

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// app.js 
+// app.js
 const express = require('express');
+const cors = require('./middlewares/cors');
 const path = require('path');
+const bodyParser = require('body-parser');
 const mainRoute = require('./routes/main');
-const gamesRouter = require('./routes/games'); 
+const gamesRouter = require('./routes/games');
 
 const PORT = 3000;
 const app = express();
 
-// Теперь клиент имеет доступ только к публичным файлам
-app.use(express.static(path.join(__dirname, 'public'))); 
-
-app.use(mainRoute, gamesRouter); 
+app.use(cors, bodyParser.json(), express.static(path.join(__dirname, 'public')), mainRoute, gamesRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running at PORT http://localhost:${PORT}`);
-})
+	console.log(`Server is running at PORT http://localhost:${PORT}`);
+});
+// мне хочется лето уже
--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,19 @@
-     // routes/games.js
-     const gamesRouter = require("express").Router(); // Создали роутер
-     const { readData, writeData } = require("../utils/data"); // Чтение и запись данных в JSON-файл
-     const {getAllGames, deleteGames} = require(../controllers/games.js)
-     gamesRouter.get("/games", getAllGames)
-     gamesRouter.delete("/games/:id", deleteGame);
-     
-     module.exports = gamesRouter;
-     
+const gamesRouter = require('express').Router();
+
+const {
+	getAllGames,
+	checkIsTitleInArray,
+	updateGamesArray,
+	updateGamesFile,
+	findGameById,
+	deleteGame
+} = require('../middlewares/games');
+const { sendAllGames, sendUpdatedGames } = require('../controllers/games');
+
+gamesRouter.get('/games', getAllGames, sendAllGames);
+
+gamesRouter.post('/games', getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames);
+
+gamesRouter.delete('/games/:id', getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
+
+module.exports = gamesRouter;
