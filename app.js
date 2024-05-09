b39ee024973d26a91fba7e9de4a820987c148932
NemontovMaksim <149705847+NemontovMaksim@users.noreply.github.com>
2024-05-09 20:29:18 +0300

    Добавил итоговый проект
    Все работает, проверял.

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// app.js
 const express = require('express');
-const cors = require('./middlewares/cors');
-const path = require('path');
 const bodyParser = require('body-parser');
-const mainRoute = require('./routes/main');
+const path = require('path');
+const usersRouter = require('./routes/users');
 const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
 
-const PORT = 3000;
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
+
 const app = express();
+const PORT = 3000;
 
-app.use(cors, bodyParser.json(), express.static(path.join(__dirname, 'public')), mainRoute, gamesRouter);
+connectToDatabase();
+
+app.use(
+  cors, 
+  bodyParser.json(),
+  express.static(path.join(__dirname, 'public')),
+  usersRouter, 
+  gamesRouter, 
+  categoriesRouter
+);
 
 app.listen(PORT, () => {
 	console.log(`Server is running at PORT http://localhost:${PORT}`);
 });
-// мне хочется лето уже
